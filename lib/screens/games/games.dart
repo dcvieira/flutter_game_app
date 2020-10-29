0687d2419bbec9fb748a20ded8dff877d668cc25
Tribunal de Justica <tribunaldejustica@Mac-mini-de-Tribunal.local>
2020-10-29 13:53:21 -0300

    part 3

--- lib/screens/games/games.dart
+++ lib/screens/games/games.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:game_app/utils/constants.dart';
+import 'package:game_app/utils/data.dart';
+
+import 'game_detail.dart';
+import 'games_all.dart';
 
 class Games extends StatefulWidget {
   @override
@@ -6,16 +11,40 @@ class Games extends StatefulWidget {
 }
 
 class _GamesState extends State<Games> {
+  Widget currentWidgetView;
+
+  @override
+  void initState() {
+    super.initState();
+    switchTo("games_all", null);
+  }
+
+  switchTo(String widgetName, Game game) {
+    setState(() {
+      switch (widgetName) {
+        case "games_all":
+          currentWidgetView = GamesAll(onPress: (Game game) {
+            switchTo("game_detail", game);
+          });
+          break;
+        case "game_detail":
+          currentWidgetView = GameDetail(
+              game: game,
+              onPress: () {
+                switchTo("games_all", null);
+              });
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          "Games",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
+    return Scaffold(
+      backgroundColor: kSecondaryColor,
+      body: AnimatedSwitcher(
+        duration: Duration(milliseconds: 250),
+        child: currentWidgetView,
       ),
     );
   }
